313dad3430ed279bcd45c6938b0501f370a672d0
BrummbaerFr <ririply@live.fr>
2016-03-06 12:25:37 +0100

    Crypter/Décrypter
    Crypter fonctionne (bug du Z à fix) :+1:
    Décrypter encore à coder
    Ajout du bouton décrypter et de l'input clé pour décrypter
    Ajout d'une variable dans cesar.js : une pour la clé de cryptage une
    pour la clé de décryptage, sinon il y a deux input HTML qui portent le
    même nom

--- hARDcipher/cesar.js
+++ hARDcipher/cesar.js
@@ -4,7 +4,7 @@ function getCesarHash(){
 
 	//Get le Plain
 	var plain = document.getElementById("plain").value;
-	var decale = document.getElementById("decale").value;
+	var decale_crypter = document.getElementById("decale_crypter").value;			//2 vars différentes sinon quand l'utilisateur décrypte le JS peut prendre la valeur qui a servi au cryptage
 	//Mettre chaque lettre dans un tableau
 	var tableau = plain.split("");
 	var hash = "";
@@ -13,7 +13,7 @@ function getCesarHash(){
 		if (tableau[i] != ' '){
 			//Parcourir le tableau en convertissant chaque lettre en ASCII et y ajouter le décalage
 			var charCode = tableau[i].charCodeAt(0);
-			var charCodeGood = Number(charCode) + Number(decale);
+			var charCodeGood = Number(charCode) + Number(decale_crypter);
 			//Remplacement du charCode par la nouvelle lettre
 			tableau[i] = String.fromCharCode(charCodeGood);
 		}
@@ -34,21 +34,22 @@ function getCesarPlain(){
 	//Mettre chaque lettre dans un tableau
 	var tableau = hash.split("");
 	var plain = "";
-	for (var decale = 1; i < tableau.length; i++) {
-		plain += 'Décalage de '+decale+': ';
+	var decale_decrypter = document.getElementById("decale_decrypter").value;
+	for (var decale_decrypter = 1; i < tableau.length; i++) {
+		plain += 'Décalage de '+decale_decrypter+': ';
 		for (var i = 0; i < tableau.length; i++) {
 			//Si nous avons à faire à un espace nous ne le traitons pas
 			if (tableau[i] != ' '){
 				//Parcourir le tableau en convertissant chaque lettre en ASCII et y retirer le décalage
 				var charCode = tableau[i].charCodeAt(0);
-				var charCodeGood = Number(charCode) - Number(decale);
+				var charCodeGood = Number(charCode) - Number(decale_decrypter);
 				//Remplacement du charCode par la nouvelle lettre
 				tableau[i] = String.fromCharCode(charCodeGood);
 			}
 			//Assemblement du tableau
 			plain += tableau[i];
 		}
-		plain += <br />;
+		plain += "<br />";
 	}
 	//On a notre list de TEXT celon le décalage
 	//Set le resultat dans le Champ plain
