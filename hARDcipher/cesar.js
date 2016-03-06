8325181bd1ac3eb68398d34c9bd21b7440e1fb50
snakypex <donovanconfais@hotmail.fr>
2016-03-06 15:38:28 +0100

    Update cesar.js

--- hARDcipher/cesar.js
+++ hARDcipher/cesar.js
@@ -15,7 +15,7 @@ function getCesarHash(){
 			var charCode = tableau[i].charCodeAt(0);
 			for (var ii = 0; ii < Number(decale_crypter); ii++) {
 				if(charCode == 90)CodeGood = 65;
-				else charCode = Number(charCode)++;
+				else charCode++;
 			}
 			//Remplacement du charCode par la nouvelle lettre
 			tableau[i] = String.fromCharCode(charCode);
