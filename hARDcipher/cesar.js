0e5875d1b3ddfb7dcd7168937ad0232d9ac37a09
snakypex <donovanconfais@hotmail.fr>
2016-03-08 15:42:09 +0100

    Update cesar.js

--- hARDcipher/cesar.js
+++ hARDcipher/cesar.js
@@ -37,7 +37,24 @@ function getCesarPlain(){
 	//Mettre chaque lettre dans un tableau
 	var tableau = hash.split("");
 	var plain = "";
-	for (var decale_decrypter = 1; decale_decrypter < 26; decale_decrypter++) {
+	if (document.getElementById("decale_decrypter") != null) {
+		var decale_decrypter = document.getElementById("decale_decrypter").value;
+		for (var i = 0; i < tableau.length; i++) {
+			var charCode = tableau[i].charCodeAt(0);
+			//Si nous avons à faire à un espace nous ne le traitons pas
+			if (tableau[i] != ' '){
+				for (var ii = 0; ii < Number(decale_decrypter); ii++) {
+					//Parcourir le tableau en convertissant chaque lettre en ASCII et y retirer le décalage
+					if(charCode == 65)charCode = 90;
+					else charCode = Number(charCode) - 1;
+				}
+			}
+			//Assemblement du tableau et Remplacement du charCode par la nouvelle lettre
+			plain += String.fromCharCode(charCode);
+		}
+	}
+	else{
+		for (var decale_decrypter = 1; decale_decrypter < 26; decale_decrypter++) {
 		plain += 'Décalage de '+decale_decrypter+': ';
 		for (var i = 0; i < tableau.length; i++) {
 			var charCode = tableau[i].charCodeAt(0);
@@ -54,7 +71,12 @@ function getCesarPlain(){
 		}
 		plain += "\n";
 	}
+	}
 	//On a notre list de TEXT celon le décalage
 	//Set le resultat dans le Champ plain
 	document.getElementById("plain").innerHTML = plain;
+}
+
+function hasKey(){
+	document.getElementById("choix").innerHTML = '<input name="decale" id="decale_decrypter" type="number"> décalage<br/>';
 }
