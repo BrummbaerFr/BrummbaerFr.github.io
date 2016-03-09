c77b3e692bec3380b300521962c93a64ee2156c9
BrummbaerFr <ririply@live.fr>
2016-03-09 16:40:38 +0100

    Début du script

--- media/js/syracuse.js
+++ media/js/syracuse.js
@@ -1,3 +1,12 @@
 function syracuseCipher() {
-  
+  var plain = document.getElementById("plain").value.toUpperCase();
+  alert(plain);
+  var plainArray = plain.split("");
+  alert(plainArray);
+    for (var i = 0; i < plainArray.length; i++) {
+      if (plainArray[i] = " ") {
+        plainArray.split(i,1);
+      }
+
+    }
 }
