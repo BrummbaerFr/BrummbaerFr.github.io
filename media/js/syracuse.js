77c181d53e04318e8b0e3113dc9b0e9150e29e13
BrummbaerFr <ririply@live.fr>
2016-03-09 21:16:02 +0100

    Update syracuse.js
    Cryptage fonctionnel, principe à changer (moteur JS plante pour coder 5
    lettres)

--- media/js/syracuse.js
+++ media/js/syracuse.js
@@ -1,30 +1,24 @@
 function syracuseCipher() {
   var plain = document.getElementById('plain').value.toUpperCase();    // on récupère le texte et on le transforme en majuscules
+  plain = plain.replace(/\s+/g, '');    // \s RegEx pour "Espace" et /g pour TOUS. Supprime tous les espaces de plain
   alert(plain);
-  var plainArray = plain.split("");   // on transforme la string contenant le texte en array
-  alert(plainArray);
   var key = "";   // var clé
-    for (var i = 0; i < plainArray.length; i++) {   // boucle de cryptage
-      if (plainArray[i] = " ") {    // si le caractère en cours de traitement est un espace, on ne fait rien
-
+  for (var i = 0; i < plain.length; i++) {   // boucle de cryptage
+    var ciphered = (plain.charCodeAt(i))-65;    // on prend le numéro ASCII de la lettre
+    while (ciphered != 1) {   // tant que l'on ne tombe pas dans le cycle 1, 4, 2 ... on applique la suite
+      if (ciphered%2 != 0) {    // si le nombre est impair, on fait les opérations nécessaires
+        ciphered *= 3;
+        ciphered += 1;
+        key += 0;   // trace (clé) 0 = impair
       }
-      else {    // sinon on applique la suite de Syracuse
-        var ciphered = (plainArray.charCodeAt(i))-65;    // on prend le numéro ASCII de la lettre
-        alert(ciphered);
-        while (ciphered != 1) {   // tant que l'on ne tombe pas dans le cycle 1, 4, 2 ... on applique la suite
-          if (ciphered%2 != 0) {    // si le nombre est impair, on fait les opérations nécessaires
-            ciphered *= 3;
-            ciphered += 1;
-            key += 0;   // trace (clé) 0 = impair
-            alert("works");
-          }
-          else {    // sinon, le nombre est forcément pair, on divise par deux
-            ciphered /= 2;
-            key += 1;   // trace (clé) 1 = pair
-          }
-        }
+      else {    // sinon, le nombre est forcément pair, on divise par deux
+        ciphered /= 2;
+        key += 1;   // trace (clé) 1 = pair
       }
-      key += 5;
     }
+    var display = "B";
+    document.getElementById('cipher').value += display;
+    key += 5;
+  }
   document.getElementById('key').value = key;
-}
+  }
