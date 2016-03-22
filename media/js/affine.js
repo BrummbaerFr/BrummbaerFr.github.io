afd8067fc89d4e94a18f816e613d8e7d44654383
snakypex <donovanconfais@hotmail.fr>
2016-03-22 16:13:33 +0100

    Update affine.js

--- media/js/affine.js
+++ media/js/affine.js
@@ -1,13 +1,17 @@
 function getAffineHash(){
 	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	var plain = document.getElementById("plain").toLowerCase();
-	var fonction = document.getElementById("fonction");
+	var plain = document.getElementById("plain").value.toLowerCase();
+	var fonction = document.getElementById("fonction").value;
 	var tableau = plain.split("");
 	var hash = "";
 	for (var i = 0; i <= tableau.length; i++) {
-		var emplacement += alphabet.indexOf(tableau[i]);
-		fonction.replace("x", emplacement);
-		var resultat = eval(fonction) % 26;
+		//Nous regardons a quel position est la lettre dans l'alphabet
+		var emplacement = alphabet.indexOf(tableau[i]);
+		//Nous remplaçons le x par l'emplacement
+		fonction_temps = fonction.replace("x", emplacement);
+		//Nous faisons le cancul
+		var resultat = eval(fonction_temps) % 26;
+		//Nous regardons a quel lettre correspond l'emplacement
 		hash += alphabet.charAt(resultat);
 	}
 	document.getElementById("hash").innerHTML = hash;
