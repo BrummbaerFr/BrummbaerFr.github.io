4a071ce27e84be82be63cf4047a0be980cba2d22
snakypex <donovanconfais@hotmail.fr>
2016-04-26 15:32:05 +0200

    Update affine.js

--- media/js/affine.js
+++ media/js/affine.js
@@ -23,5 +23,24 @@ function getAffineHash(){
 }
 
 function getPlainByAffine(){
-	
+	var affine = document.getElementById("hash").value.toLowerCase();
+	var fonction = document.getElementById("fonction").value.replace("+", "-").replace("-", "+");
+	var tableau = affine.split("");
+	var plain = "";
+	for (var i = 0; i <= tableau.length; i++) {
+		if(tableau[i] != " "){
+			//Nous regardons a quel position est la lettre dans l'alphabet
+			var emplacement = alphabet.indexOf(tableau[i]) + 1;
+			//Nous remplaçons le x par l'emplacement
+			var fonction_temps = fonction.replace("x", emplacement);
+			//Nous faisons le cancul
+			var resultat = eval(fonction_temps) % 26;
+			//Nous regardons a quel lettre correspond l'emplacement
+			plain += alphabet.charAt(resultat - 1);
+		}
+		else{
+			plain += " ";
+		}
+	}
+	document.getElementById("plain").innerHTML = plain;
 }
