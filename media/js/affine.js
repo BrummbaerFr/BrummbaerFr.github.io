627f2fe66b49d7a8aa236e817e5e94e98dd0da48
snakypex <donovanconfais@hotmail.fr>
2016-05-22 14:53:30 +0200

    Fini

--- media/js/affine.js
+++ media/js/affine.js
@@ -1,9 +1,13 @@
-function getAffineHash(){
+function load(){
 	document.getElementById("plain").style.textTransform = "uppercase"; //Je transforme l'affichage du texte du champs en majuscule
-	
+	document.getElementById("affine").style.textTransform = "uppercase";
+}
+
+function getAffineHash(){
 	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	var plain = RemoveAccents(document.getElementById("plain").value.toUpperCase()); //On get la variable en la mettant en maj
-	plain = plain.replace(/[^A-Z ]+/g, ""); //On supprime tout ce qui ne fais pas partie de l'alphabet
+	var plain = RemoveAccents(document.getElementById("plain").value.toLowerCase());
+	plain = plain.replace(/[^a-zA-Z ]+/g, "");
+	document.getElementById("plain").value = plain;
 	var fonction = document.getElementById("fonction").value;
 	var tableau = plain.split("");
 	var hash = "";
@@ -16,7 +20,8 @@ function getAffineHash(){
 			//Nous faisons le cancul
 			var resultat = eval(fonction_temps) % 26;
 			//Nous regardons a quel lettre correspond l'emplacement
-			hash += alphabet.charAt(resultat - 1);
+			if(resultat == 0)hash += "A";
+			else hash += alphabet.charAt(resultat - 1);
 		}
 		else{
 			hash += " ";
@@ -25,32 +30,6 @@ function getAffineHash(){
 	document.getElementById("affine").value = hash;
 }
 
-function getPlainByAffine(){
-	document.getElementById("affine").style.textTransform = "uppercase";
-	
-	var alphabet = "abcdefghijklmnopqrstuvwxyz";
-	var affine = RemoveAccents(document.getElementById("affine").value.toUpperCase());
-	affine = affine.replace(/[^A-Z ]+/g, ""); //On supprime tout ce qui ne fais pas partie de l'alphabet
-	var fonction = document.getElementById("fonction").value.replace("+", "-").replace("-", "+");
-	var tableau = affine.split("");
-	var plain = "";
-	for (var i = 0; i < tableau.length; i++) {
-		if(tableau[i] != " "){
-			//Nous regardons a quel position est la lettre dans l'alphabet
-			var emplacement = alphabet.indexOf(tableau[i]) + 1;
-			//Nous remplaçons le x par l'emplacement
-			var fonction_temps = fonction.replace("x", emplacement);
-			//Nous faisons le cancul
-			var resultat = eval(fonction_temps) % 26;
-			//Nous regardons a quel lettre correspond l'emplacement
-			plain += alphabet.charAt(resultat - 1);
-		}
-		else{
-			plain += " ";
-		}
-	}
-	document.getElementById("plain").value = plain;
-}
 function RemoveAccents(strAccents) {
 		//MERCI A alisterlf POUR AVOIR FAIT CE SCRIPT ENUMERANT TOUT LES CARACTERES AVEC ACCENTS
 		var strAccents = strAccents.split('');
